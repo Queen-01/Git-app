1a07ceb660821b779f26f909935df84119d0804f
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-05-22 23:41:12 +0300

    deleted some folders

--- src/app/git-user.ts
+++ src/app/git-user.ts
@@ -1,3 +1,11 @@
+import { Url } from 'url';
+
 export class GitUser {
-    
+  constructor(
+    public following: number,
+    public followers: number,
+    public login: string,
+    public avatar_url: string,
+
+  ) {}
 }
