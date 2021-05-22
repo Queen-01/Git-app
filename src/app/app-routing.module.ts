35dfcf6b3821847a092abc48b1361275c15a7d5e
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-05-23 00:01:08 +0300

    add changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 const routes: Routes = [
-  {path: '', component:UserProfileComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { SearchComponent } from './search/search.component';
-
+import { RepositoryComponent } from './repository/repository.component';
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent,
-    SearchComponent
+    RepositoryComponent,
   ],
   imports: [
     BrowserModule,
