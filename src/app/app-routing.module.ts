8243f2e3e2de4bc110131aabe4b498f3bf3b2a69
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-05-23 01:00:30 +0300

    add service file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-
+import { RepositoryComponent } from './repository/repository.component';
 const routes: Routes = [
+  {path:'', component:RepositoryComponent}
 ];
 
 @NgModule({
--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Users } from '../users'
 @Component({
   selector: 'app-repository',
   templateUrl: './repository.component.html',
   styleUrls: ['./repository.component.css']
 })
 export class RepositoryComponent implements OnInit {
+  user!: Users;
 
   constructor() { }
 
