8404498c655db2f8ad5020ced67b78e4f52ebb12
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-05-23 10:08:39 +0300

    add changes

--- src/app/repo/git.service.ts
+++ src/app/repo/git.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { Users } from '../users'
+import { HttpClientModule } from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class GitService {
+  searchUser!: Users;
 
-  constructor() { }
+  constructor(private http:HttpClientModule) {
+    this.searchUser= new Users("", "", "", "" ,0, 0, 0, new Date)
+   }
 }
--- src/app/users.ts
+++ src/app/users.ts
@@ -1,3 +1,11 @@
-export class Users{
-    
+export class Users {
+    constructor(
+        public name:string,
+        public login:string,
+        public avatar_url:string,
+        public public_repos:number,
+        public followers:number,
+        public following: number,
+        public created-on: Date
+    )
 }
