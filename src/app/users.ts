3afa56461cedfb9d71637797272a6056f5a18e88
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-05-23 14:10:42 +0300

    add files

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,11 +1,10 @@
 export class Users {
-    constructor(
-        public name:string,
-        public login:string,
-        public avatar_url:string,
-        public public_repos:number,
-        public followers:number,
-        public following: number,
-        public created-on: Date
-    )
+  constructor(
+    public login: string,
+    public avatar_url: string,
+    public public_repos: number,
+    public following: number,
+    public followers: number,
+    public created_On: Date
+  ){};
 }
