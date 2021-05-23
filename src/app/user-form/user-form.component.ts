24a7f939c603891c48d6c7fd16297694ce83995a
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-05-24 01:32:17 +0300

    add user form

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { GitService } from '../repo/git.service'
 
 @Component({
   selector: 'app-user-form',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
+  username: string=''
 
-  constructor() { }
+  @Output() searchTerm= new EventEmitter()
+  constructor(private repoService: GitService) { }
 
   ngOnInit(): void {
   }
+
+  search(){
+    this.searchTerm.emit(this.username);
+    this.username = "";
+  }
+  
 
 }
